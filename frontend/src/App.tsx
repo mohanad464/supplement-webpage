ba21f039005515aa6a58bfe95b434839ecdcd19a
mohanad <mohanad_u@yahoo.com>
2024-10-02 07:51:38 +0300

    Scafolding frontend folder

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Herosection from "./components/Herosection";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Herosection />
+
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Herosection.tsx
@@ -0,0 +1,9 @@
+
+
+const Herosection = () => {
+  return (
+    <div>Herosection</div>
+  )
+}
+
+export default Herosection
