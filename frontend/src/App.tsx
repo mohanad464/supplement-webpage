2dfcbc61e9ff316a0c22ade1a347c40b92de4ecf
mohanad <mohanad_u@yahoo.com>
2024-10-03 08:27:21 +0300

    pages implementation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
+import "./index.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Herosection from "./components/Herosection";
-import "./index.css";
 
 const App = () => {
   return (
--- frontend/src/pages/Cart.tsx
+++
@@ -1,5 +0,0 @@
-const Cart = () => {
-  return <button>Cart</button>;
-};
-
-export default Cart;
