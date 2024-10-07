e656ae50d05dc456eb8b94a70f1d1a3296c8bc49
mohanad <mohanad_u@yahoo.com>
2024-10-07 09:14:58 +0300

    Add/cart/ GET endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import userRoute from "./routes/userRoute";
 import { seedIntialProducts } from "./services/productService";
 import productRoute from "./routes/productRoute";
+import cartRoute from "./routes/cartRoute"
 
 const app = express();
 const port = 3003;
@@ -19,6 +20,7 @@ seedIntialProducts();
 
 app.use("/user", userRoute);
 app.use("/product", productRoute);
+app.use("/cart", cartRoute)
 
 app.listen(port, () => {
   console.log(`Server is running at: http://localhost:${port}`);
--- backend/src/services/userService.ts
+++ backend/src/services/userService.ts
@@ -1,6 +1,6 @@
 import userModel from "../models/userModel";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 interface RegisterParams {
   firstName: string;
@@ -21,11 +21,16 @@ export const register = async ({
     return { data: "User already exists!", statusCode: 400 };
   }
 
-  const hashedPassword = await bcrypt.hash(password, 10)
-  const newUser = new userModel({ firstName, lastName, email, password:hashedPassword });
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new userModel({
+    firstName,
+    lastName,
+    email,
+    password: hashedPassword,
+  });
   await newUser.save();
 
-  return { data: generateJWT({ firstName, lastName, email}), statusCode: 200 };
+  return { data: generateJWT({ firstName, lastName, email }), statusCode: 200 };
 };
 
 interface LoginParams {
@@ -37,17 +42,24 @@ export const login = async ({ email, password }: LoginParams) => {
   const findUser = await userModel.findOne({ email });
 
   if (!findUser) {
-    return { data: "Incorrect email or password!", statusCode:400 } ;
+    return { data: "Incorrect email or password!", statusCode: 400 };
   }
 
   const passwordMatch = await bcrypt.compare(password, findUser.password);
   if (passwordMatch) {
-    return { data: generateJWT({email, firstName: findUser.firstName, lastName: findUser.lastName}), statusCode: 200};
+    return {
+      data: generateJWT({
+        email,
+        firstName: findUser.firstName,
+        lastName: findUser.lastName,
+      }),
+      statusCode: 200,
+    };
   }
 
   return { data: "Incorrect email or password!", statusCode: 400 };
 };
 
 const generateJWT = (data: any) => {
-    return jwt.sign(data, "tsMmZKkTqZ4vB0hZRdCwXy9Me8eHAxeH")
-}
+  return jwt.sign(data, "tsMmZKkTqZ4vB0hZRdCwXy9Me8eHAxeH");
+};
